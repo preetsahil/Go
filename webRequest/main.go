618b8f094677c49729355ec5763a7c0a3abbc8af
sahil <preetsahil289@gmail.com>
2024-01-15 22:54:18 +0530

    Handling web request in go

---
+++ webRequest/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+const url="http://lco.dev"
+func main()  {
+	fmt.Println("Welcome to web Request")
+
+	response,err :=http.Get(url)
+
+	if err!=nil{
+		panic(err)
+	}
+
+	fmt.Printf("Response type is %T\n",response)
+
+	defer response.Body.Close() 
+
+	//read response
+
+	datatype,err := io.ReadAll(response.Body)
+
+	content :=string(datatype)
+
+	fmt.Println(content)
+	
+}
